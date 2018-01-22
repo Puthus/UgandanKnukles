fc05a70de574ead77af6c74683f62314f0a5c1fa
Soulayman <syco09@outlook.com>
2018-01-22 04:26:35 +0000

    kinda finished ?

--- js/jil.js
+++ js/jil.js
@@ -1,21 +1,63 @@
-$(document).scroll(function(){
-	"use strict";
-	handleTopNavAnimation();
+$(window).on("load",(function() {
+    "use strict";
+	NavAnimation();
+}));
+
+$(window).scroll(function() {
+    "use strict";
+	console.log("scroll");
+	NavAnimation();
 });
 
-$(document).load(function(){
-	"use strict";
-	handleTopNavAnimation();
-});
-
-function handleTopNavAnimation() {
-	"use strict";
-	var top=$(document).scrollTop();
-
-	if(top>10){
-		$('#site-nav').addClass('navbar-solid'); 
-	}
-	else{
-		$('#site-nav').removeClass('navbar-solid'); 
-	}
+function NavAnimation() {
+    "use strict";
+    var top = $(document).scrollTop();
+    if (top > 10) {
+         $("#site-nav").addClass("navbar-solid");
+    } else {
+        $("#site-nav").removeClass("navbar-solid");
+    }
 }
+
+var deadline = '2018-3-18';
+initializeClock('clockdiv', deadline);
+
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
