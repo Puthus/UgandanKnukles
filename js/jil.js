41188d1b0278601aa7a8114122550ab0edbbab01
Soulayman <syco09@outlook.com>
2018-01-22 22:57:45 +0000

    map

--- js/jil.js
+++ js/jil.js
@@ -63,3 +63,15 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
+
+function initMap() {
+            var uluru = { lat: -25.363, lng: 131.044 };
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 4,
+                center: uluru
+            });
+            var marker = new google.maps.Marker({
+                position: uluru,
+                map: map
+            });
+        }
